fbf2e76b2ac99689ddbadbed81b4b55395050b52
Raymond Zhang <157990153+RaymondZhang33@users.noreply.github.com>
2025-10-28 13:24:14 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://learncc.vercel.app/learn"
+            href="/learn"
             target="_blank"
             rel="noopener noreferrer"
           >
