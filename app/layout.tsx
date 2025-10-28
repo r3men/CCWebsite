bc5582c771e9e956e46696cf2b05f362036cef59
Raymond Zhang <157990153+RaymondZhang33@users.noreply.github.com>
2025-10-28 13:26:29 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Learn Cybersecurity",
   description: "Template from Vercel.",
-  icon: "/favicon.png",
+  icons: "/favicon.png",
 };
 
 export default function RootLayout({
